f09fdefdd2bd0871dd6b62e4c46893fd65125f71
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-06-13 23:08:19 +0300

    DiscArray tests

--- test/test_disc.js
+++ test/test_disc.js
@@ -1,8 +1,10 @@
 'use strict'
 
 let assert = require('assert')
+let util = require('util')
 
-let Disc = require('../dump2dvd').Disc
+let Disc = require('../disc-calc').Disc
+let DiscArray = require('../disc-calc').DiscArray
 
 suite('Disc', function() {
 
@@ -25,6 +27,35 @@ suite('Disc', function() {
 	assert(d.add_partial({ name: 'q', size: 10 }, 0, 3))
 	assert.equal(false, d.add_partial({ name: 'w', size: 10 }, 0, 6))
 //	console.log(d)
+    })
+
+})
+
+suite('DiscArray', function() {
+
+    test('save 3 files, 2 of which fit in 1 disc', function() {
+	let da = new DiscArray(4)
+	da.save({ name: 'small-1', size: 2 })
+	da.save({ name: 'big', size: 3 })
+	da.save({ name: 'small-2', size: 2 })
+
+	assert.equal(0, da.storage[0].free)
+	assert.equal(2, da.storage[0].files.length)
+	assert.equal('big', da.storage[1].files[0].name)
+
+//	console.log(util.inspect(da, {depth: null}))
+    })
+
+    test('save 1 big file that is too big to fit in 1 disc', function() {
+	let da = new DiscArray(4)
+	da.save({ name: 'big', size: 6 })
+
+	assert.equal(0, da.storage[0].files[0].pos_from)
+	assert.equal(3, da.storage[0].files[0].pos_to)
+	assert.equal(4, da.storage[1].files[0].pos_from)
+	assert.equal(5, da.storage[1].files[0].pos_to)
+
+//	console.log(util.inspect(da, {depth: null}))
     })
 
 })
