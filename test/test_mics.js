493895ceff05079c307d95007e09bfc01e6ee98b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-06-15 02:28:26 +0300

    fix tests

--- test/test_mics.js
+++ test/test_mics.js
@@ -10,7 +10,7 @@ suite('NumberParser', function() {
     test('parse', function() {
 	assert.equal(1, NumberParser.parse('1'))
 	assert.equal(111, NumberParser.parse('1,1.1 '))
-	assert.equal(25025314816, NumberParser.parse(' bd  '))
+	assert.equal(25024000000, NumberParser.parse(' bd  '))
     })
 
     test('parse invalid', function() {
