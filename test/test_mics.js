6e67602a752e0567a768f931f9491de3f8f2acf7
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-06-14 04:34:37 +0300

    add NumberParser

---
+++ test/test_mics.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let assert = require('assert')
+let util = require('util')
+
+let NumberParser = require('../disc-calc').NumberParser
+
+suite('NumberParser', function() {
+
+    test('parse', function() {
+	assert.equal(1, NumberParser.parse('1'))
+	assert.equal(111, NumberParser.parse('1,1.1 '))
+	assert.equal(25025314816, NumberParser.parse(' bd  '))
+    })
+
+    test('parse invalid', function() {
+	assert.throws( ()=> NumberParser.parse('') )
+	assert.throws( ()=> NumberParser.parse(', ') )
+	assert.throws( ()=> NumberParser.parse(' d ') )
+    })
+
+})
